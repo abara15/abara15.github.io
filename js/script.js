7de108b5b57d263676f91cd761f17854d5d2d958
abara15 <61340421+abara15@users.noreply.github.com>
2021-06-29 13:22:29 +1000

    navigation: got scroll js done and main design. Need to fix mobile version

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.backgroundColor = "#16161D";
+        document.getElementById("header").style.paddingTop = "30px";
+        document.getElementById("header").style.paddingBottom = "30px";
+    } else {
+        document.getElementById("header").style.backgroundColor = "transparent";
+    }
+}
