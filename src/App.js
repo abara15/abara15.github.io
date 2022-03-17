560076caa1af2b619e2ae2240b13d82c03ee52bc
abara15 <61340421+abara15@users.noreply.github.com>
2022-03-18 00:33:11 +1100

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { Divider, Navbar, SocialMedia } from "./components";
+import { About, Footer, Header, Skills, Work } from "./container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="app">
+			<SocialMedia />
+			<Navbar />
+			<Header />
+			<About />
+			<Divider />
+			<Skills />
+			<Divider />
+			<Work />
+			<Divider />
+			<Footer />
+			<div className="copyright">
+				<p className="p-text">&copy; '22 ABARA</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
