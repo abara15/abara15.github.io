f4653d6ed4c43bebcec3e3543d5c08f79173280d
abara15 <61340421+abara15@users.noreply.github.com>
2022-03-18 09:19:22 +1100

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Divider } from "./Divider/Divider";
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as NavigationDots } from "./NavigationDots/NavigationDots";
-export { default as SocialMedia } from "./SocialMedia/SocialMedia";
